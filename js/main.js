a828b1c2682705125bc0bdb3570de8bb4106b8c9
aicha <dyssid0722@gmail.com>
2022-12-16 18:10:01 +0900

    survey 영역 수정
    survey 좌우 노출 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -54,3 +54,17 @@ window.addEventListener('scroll', function () {
     });
   }
 });
+
+// 좌우 나타나는 애니메이션
+const spyEls= document.querySelectorAll('section.scroll-spy');
+
+console.log(spyEls);
+// 메소드 체이닝
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 감시할 요소를 지정
+    triggerHook: 0.8 // 화면의 80% 지점에서 보여짐 여부 감시(0~1 사이 지정)
+  })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당
+});
