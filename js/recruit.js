9b721fe0ac254e3c8fbfc7775562ff224e4128b1
aicha <dyssid0722@gmail.com>
2023-01-11 18:41:35 +0900

    greeting페이지/ 로고 메인페이지로 연결/ recruit.js에 이메일 입력란 확인 동작 추가

--- js/recruit.js
+++ js/recruit.js
@@ -15,3 +15,42 @@ const recruitSignDate = document.querySelector('.sign__dd');
 recruitSignYear.textContent = new Date().getFullYear();
 recruitSignMonth.textContent = new Date().getMonth();
 recruitSignDate.textContent = new Date().getDate();
+
+
+
+function CheckEmail(str){                                                 
+  var reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+  
+  if(!reg_email.test(str)) {  // 정규표현식 메소드
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+// console.log(typeof reg_email);
+
+
+// 로그인 버튼을 클릭했을 때 실행되는 함수                                 
+function GoToEnroll() {                                          
+	var obEmail = document.getElementById("re_email");
+	if (!obEmail.value) {
+    // debugger
+		alert("이메일을 입력하세요!");
+		obEmail.focus();
+		return;
+	}               
+	else   {          
+		if(!CheckEmail(obEmail.value))	{
+			alert("이메일 형식이 잘못되었습니다");
+			obEmail.focus();
+			return;
+		}                
+	}                      
+}                           
+
+// 이메일을 입력받는 태그           
+// <input type="text" id = "email" name = "email" >
+
+// 회원가입 버튼 
+// <button OnClick = "javascript:GoToEnroll()">회원가입</button>
